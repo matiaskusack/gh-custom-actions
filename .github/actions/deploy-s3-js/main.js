f46a0d71732a5f177dd57936da34954bb543013a
Matías Kusack <matias.kusack@synit.es>
2026-03-23 16:19:09 +0100

    Tested another way

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,6 +1,5 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const exec = require('@actions/exec');
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 function run(){
     core.notice('Hello from deploy-s3-js action!');
