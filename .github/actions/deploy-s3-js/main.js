bc2c1d8687c2a5fdec188fec2eff6f5c5c013598
Matías Kusack <matias.kusack@synit.es>
2026-03-23 16:38:25 +0100

    S3 deployment

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,8 +1,20 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import * as exec from "@actions/exec";
 
 function run(){
     core.notice('Hello from deploy-s3-js action!');
+
+    const bucket = core.getInput('bucket-name', { required: true });
+    const region = core.getInput('region', { required: true });
+    const sourceDir = core.getInput('source-dir', { required: true });
+
+    const context = github.context;
+    core.notice(`Uploading to ${region} bucket ${bucket} from ${sourceDir}...`);
+
+    const s3Uri = `s3://${bucket}`;
+    // exec.exec(`aws s3 sync ${sourceDir} ${s3Uri} --region ${region}`);
+    core.notice(`Upload complete to ${s3Uri}!`);
 }
 
 run();
